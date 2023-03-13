1434dd1402507dea3a6f398cb6c420727eed607f
Catherine <catherine.wambui@student.moringaschool.com>
2023-03-13 12:19:02 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+//let currentUser = "Catherine";
+//let welcomeMessage = "Welcome to Flatbook, ";
+//let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+//console.log(welcomeMessage);
+
+const currentUser = "Catherine";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//console.log(excitedWelcomeMessage);
+
+//const shortGreeting = "Welcome, ";
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+//console.log(shortGreeting.length);
+
+
+
+
